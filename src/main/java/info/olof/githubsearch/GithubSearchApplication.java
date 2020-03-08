6c0f031e7b36a5ad5726977424e71131f0717334
Olof Nord <olof.nord@tutanota.com>
2020-03-08 22:12:21 +0100

    Add initial Spring Boot project based on Spring Initializr

---
+++ src/main/java/info/olof/githubsearch/GithubSearchApplication.java
@@ -0,0 +1,13 @@
+package info.olof.githubsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSearchApplication.class, args);
+	}
+
+}
