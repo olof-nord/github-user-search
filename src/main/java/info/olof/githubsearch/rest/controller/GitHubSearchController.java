d5d3583abad0f5c9e4c65e15d9a3eee97d05e2ab
Olof Nord <olof.nord@tutanota.com>
2020-03-09 23:28:48 +0100

    Allow entering several programming languages as search parameter

--- src/main/java/info/olof/githubsearch/rest/controller/GitHubSearchController.java
+++ src/main/java/info/olof/githubsearch/rest/controller/GitHubSearchController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class GitHubSearchController {
@@ -18,9 +20,9 @@ public class GitHubSearchController {
     private static final Logger LOGGER = LoggerFactory.getLogger(GitHubSearchController.class);
 
     @GetMapping("/search")
-    public ResponseEntity<SearchResultDTO> search(@RequestParam String username, @RequestParam String programmingLanguage) {
+    public ResponseEntity<SearchResultDTO> search(@RequestParam String username, @RequestParam(name = "programming_language") List<String> programmingLanguages) {
 
-        LOGGER.info("search request: username: {}, programming language: {}", username, programmingLanguage);
+        LOGGER.info("search request: username: {}, programming language(s): {}", username, programmingLanguages);
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(SearchResultDTO.builder()
