d098f2417a2001c8d9131f14c5e54ad84ec2ca5b
Olof Nord <olof.nord@tutanota.com>
2020-03-11 00:06:41 +0100

    Start using the GitHub Search API
    Add a service which uses the generated GitHub reactive client to search
    Adjust the GitHub API specification to use simpler object types to not run into parsing issues
    Add GitHub access token management with a .env file

--- src/main/java/info/olof/githubsearch/rest/controller/GitHubSearchController.java
+++ src/main/java/info/olof/githubsearch/rest/controller/GitHubSearchController.java
@@ -1,6 +1,8 @@
 package info.olof.githubsearch.rest.controller;
 
 import info.olof.githubsearch.rest.dto.SearchResultDTO;
+import info.olof.githubsearch.service.GitHubSearchService;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -14,15 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class GitHubSearchController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GitHubSearchController.class);
 
+    private GitHubSearchService gitHubSearchService;
+
     @GetMapping("/search")
     public ResponseEntity<SearchResultDTO> search(@RequestParam String username, @RequestParam(name = "programming_language") List<String> programmingLanguages) {
 
-        LOGGER.info("search request: username: {}, programming language(s): {}", username, programmingLanguages);
+        LOGGER.info("search input: username: {}, programming language(s): {}", username, programmingLanguages);
+
+        gitHubSearchService.searchGitHubRepositories(username, programmingLanguages);
 
         return ResponseEntity.status(HttpStatus.OK)
             .body(SearchResultDTO.builder()
---
+++ src/main/java/info/olof/githubsearch/service/GitHubSearchService.java
@@ -0,0 +1,71 @@
+package info.olof.githubsearch.service;
+
+import info.olof.githubsearch.generated.api.GitHubSearchApi;
+import info.olof.githubsearch.generated.model.RepositorySearch;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.reactivex.Observable;
+import io.reactivex.observers.DisposableObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
+
+@Service
+public class GitHubSearchService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GitHubSearchService.class);
+    private static final String ACCEPT_HEADER = "application/vnd.github.mercy-preview+json";
+    private GitHubSearchApi service;
+    private String accessToken;
+
+    public GitHubSearchService() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build();
+
+        service = retrofit.create(GitHubSearchApi.class);
+
+        Dotenv dotenv = Dotenv.load();
+        accessToken = "token " + dotenv.get("GITHUB_ACCESS_TOKEN");
+    }
+
+    public void searchGitHubRepositories(String username, List<String> programmingLanguages) {
+        String searchQuery = username + " language:" + programmingLanguages.get(0);
+
+        LOGGER.info("GitHub search query: {}", searchQuery);
+
+        Observable<RepositorySearch> repositories = service.getRepositories(
+            accessToken,
+            searchQuery,
+            ACCEPT_HEADER,
+            "stars",
+            "desc"
+            );
+
+        repositories.subscribeWith(new DisposableObserver<>() {
+            @Override
+            public void onNext(RepositorySearch repositorySearch) {
+                LOGGER.info("onNext {}", repositorySearch);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                LOGGER.info("onError");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                LOGGER.info("onComplete");
+            }
+        });
+
+    }
+}
